f8c5b1741f2497d5902de4684f913c327a5146c8
Skeletonxf <skeletonxf@gmail.com>
2022-12-02 22:16:42 +0000

    Add tensor variant doc example/test for logistic regression

--- src/logistic_regression.rs
+++ src/logistic_regression.rs
@@ -70,6 +70,9 @@ The update rule:
 **w<sub>new</sub>** = **w<sub>old</sub>** + learning_rate * (the sum over all i data of (y<sub>i</sub> - [1 / (1 + e^(-(**w**<sup>T</sup>**x**)))])**x<sub>i</sub>**))
 
 # Logistic regression example
+
+## Matrix APIs
+
 ```
 // Actual types of datasets logistic regression might be performed on include diagnostic
 // datasets such as cancer/not cancer diagnosis and various measurements of patients.
@@ -297,6 +300,273 @@ fn decision_boundry(weights: &Matrix<f64>) {
         .map(|(x1, x2)| {
             let input = Matrix::column(vec![ 1.0, x1, x2 ]);
             let prediction = sigmoid((weights.transpose() * input).scalar());
+            return if prediction > 0.5 {
+                (x1, x2, 1)
+            } else {
+                (x1, x2, 0)
+            }
+        })
+        .filter(|(_, _, class)| class == &1)
+        .map(|(x1, x2, _)| (x1 as f32, x2 as f32))
+        .collect::<Vec<(f32, f32)>>();
+    Chart::new(180, 60, -8.0, 8.0)
+        .lineplot(Shape::Points(&points))
+        .display();
+}
+```
+
+## Tensor APIs
+```
+// Actual types of datasets logistic regression might be performed on include diagnostic
+// datasets such as cancer/not cancer diagnosis and various measurements of patients.
+// More abstract datasets could be related to coin flipping.
+//
+// To ensure our example is not overly complicated but requires two dimensions for the inputs
+// to estimate the probability distribution of the random variable we sample from, we model
+// two clusters from different classes to classify. As each class is arbitary, we assign the first
+// class as the True case that the model should predict >0.5 probability for, and the second
+// class as the False case that the model should predict <0.5 probability for.
+use easy_ml::tensors::Tensor;
+use easy_ml::distributions::MultivariateGaussianTensor;
+
+use rand::{Rng, SeedableRng};
+use rand::distributions::{DistIter, Standard};
+use rand_chacha::ChaCha8Rng;
+
+use textplots::{Chart, Plot, Shape};
+
+// use a fixed seed random generator from the rand crate
+let mut random_generator = ChaCha8Rng::seed_from_u64(13);
+
+// define two cluster centres using two 2d gaussians, making sure they overlap a bit
+let class1 = MultivariateGaussianTensor::new(
+    Tensor::from([("means", 2)], vec![ 2.0, 3.0 ]),
+    Tensor::from(
+        [("rows", 2), ("columns", 2)],
+        vec![
+            1.0, 0.1,
+            0.1, 1.0
+        ]
+    )
+).unwrap(); // unwrapping here is fine because this is constructed from a known valid input
+
+// make the second cluster more spread out so there will be a bit of overlap with the first
+// in the (0,0) to (1, 1) area
+let class2 = MultivariateGaussianTensor::new(
+    Tensor::from([("means", 2)], vec![ -2.0, -1.0 ]),
+    Tensor::from(
+        [("rows", 2), ("columns", 2)],
+        vec![
+            2.5, 1.2,
+            1.2, 2.5
+        ]
+    )
+).unwrap(); // unwrapping here is fine because this is constructed from a known valid input
+
+// Generate 200 points for each cluster
+let points = 200;
+let mut random_numbers: DistIter<Standard, &mut ChaCha8Rng, f64> =
+    (&mut random_generator).sample_iter(Standard);
+// we can unwrap here because we deliberately constructed a positive definite covariance matrix
+// and supplied enough random numbers
+let class1_points = class1.draw(&mut random_numbers, points, "data", "feature").unwrap();
+let class2_points = class2.draw(&mut random_numbers, points, "data", "feature").unwrap();
+
+// Plot each class of the generated data in a scatter plot
+println!("Generated data points");
+
+/**
+ * Helper function to print a scatter plot of a provided matrix with x, y in each row
+ */
+fn scatter_plot(data: &Tensor<f64, 2>) {
+    // textplots expects a Vec<(f32, f32)> where each tuple is a (x,y) point to plot,
+    // so we must transform the data from the cluster points slightly to plot
+    let scatter_points = data
+        .select([("feature", 0)])
+        .iter()
+        // zip is used to merge the x and y columns in the data into a single tuple
+        .zip(data.select([("feature", 1)]).iter())
+        // finally we map the tuples of (f64, f64) into (f32, f32) for handing to the library
+        .map(|(x, y)| (x as f32, y as f32))
+        .collect::<Vec<(f32, f32)>>();
+    Chart::new(180, 60, -8.0, 8.0)
+        .lineplot(Shape::Points(&scatter_points))
+        .display();
+}
+
+println!("Classs 1");
+scatter_plot(&class1_points);
+println!("Classs 2");
+scatter_plot(&class2_points);
+
+// for ease of use later we insert a 0th column into both class's points so w0 + w1*x1 + w2*x2
+// can be computed by w^T x
+fn design_matrix(points: &Tensor<f64, 2>) -> Tensor<f64, 2> {
+    let mut design_matrix = Tensor::empty(
+        [("data", points.shape()[0].1), ("feature", 3)],
+        1.0
+    );
+    let mut data = points.iter();
+    for ([_row, feature], x) in design_matrix.iter_reference_mut().with_index() {
+        *x = match feature {
+            0 => 1.0,
+            1 | 2 | _ => data.next().unwrap(),
+        };
+    }
+    design_matrix
+}
+let class1_inputs = design_matrix(&class1_points);
+let class2_inputs = design_matrix(&class2_points);
+
+/**
+ * The sigmoid function, taking values in the [-inf, inf] range and mapping
+ * them into the [0, 1] range.
+ */
+fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + ((-x).exp()))
+}
+
+/**
+ * The logit function, taking values in the [0, 1] range and mapping
+ * them into the [-inf, inf] range
+ */
+fn logit(x: f64) -> f64 {
+    (x / (1.0 - x)).ln()
+}
+
+// First we initialise the weights matrix to some initial values
+let mut weights = Tensor::from([("weights", 3)], vec![ 1.0, 1.0, 1.0 ]);
+
+/**
+ * The log of the likelihood function P(**y**|X). This is what we want to update our
+ * weights to maximise as we want to train the model to predict y given **x**,
+ * where y is the class and **x** is the two features the model takes as input.
+ * It should be noted that something has probably gone wrong if you ever get 100%
+ * performance on your training data, either your training data is linearly seperable
+ * or you are overfitting and the weights won't generalise to predicting the correct
+ * class given unseen inputs.
+ *
+ * This function is mostly defined for completeness, we maximise it using the derivative
+ * and never need to compute it.
+ */
+fn log_likelihood(
+    weights: &Tensor<f64, 1>, class1_inputs: &Tensor<f64, 2>, class2_inputs: &Tensor<f64, 2>
+) -> f64 {
+    // The probability of predicting all inputs as the correct class is the product
+    // of the probability of predicting each individal input and class correctly, as we
+    // assume each sample is independent of each other.
+    let mut likelihood = 1_f64.ln();
+    // the model should predict 1 for each class 1
+    let predictions = (class1_inputs * weights.expand([(1, "columns")])).map(sigmoid);
+    for x in predictions.iter() {
+        likelihood += x.ln();
+    }
+    // the model should predict 0 for each class 2
+    let predictions = (class2_inputs * weights.expand([(1, "columns")])).map(sigmoid).map(|p| 1.0 - p);
+    for x in predictions.iter() {
+        likelihood += x.ln();
+    }
+    likelihood
+}
+
+/**
+ * The derivative of the negative log likelihood function, which we want to set to 0 in order
+ * to maximise P(**y**|X).
+ */
+fn update_function(
+    weights: &Tensor<f64, 1>, class1_inputs: &Tensor<f64, 2>, class2_inputs: &Tensor<f64, 2>
+) -> Tensor<f64, 1> {
+    let mut derivative = Tensor::from([("weights", 3)], vec![ 0.0, 0.0, 0.0 ]);
+
+    // compute y - predictions for all the first class of inputs
+    let prediction_errors = (class1_inputs * weights.expand([(1, "columns")]))
+        .map(sigmoid).map(|p| 1.0 - p);
+    for ([row, _feature], diff) in prediction_errors.iter().with_index() {
+        // compute diff * x_i
+        let mut ith_error = class1_inputs
+            .select([("data", row)])
+            .map(|x| x * diff);
+        ith_error.rename(["weights"]);
+        derivative = derivative + ith_error;
+    }
+
+    // compute y - predictions for all the second class of inputs
+    let prediction_errors = (class2_inputs * weights.expand([(1, "columns")]))
+        .map(sigmoid).map(|p| 0.0 - p);
+    for ([row, _feature], diff) in prediction_errors.iter().with_index() {
+        // compute diff * x_i
+        let mut ith_error = class2_inputs
+            .select([("data", row)])
+            .map(|x| x * diff);
+        ith_error.rename(["weights"]);
+        derivative = derivative + ith_error;
+    }
+
+    derivative
+}
+
+let learning_rate = 0.002;
+
+let mut log_likelihood_progress = Vec::with_capacity(25);
+
+// For this example we cheat and have simply found what number of iterations and learning rate
+// yields a correct decision boundry so don't actually check for convergence. In a real example
+// you would stop once the updates for the weights become 0 or very close to 0.
+for i in 0..25 {
+    let update = update_function(&weights, &class1_inputs, &class2_inputs);
+    weights = weights + (update.map(|w| w * learning_rate));
+    log_likelihood_progress.push(
+        (i as f32, log_likelihood(&weights, &class1_inputs, &class2_inputs) as f32)
+    );
+}
+
+println!("Log likelihood over 25 iterations (bigger is better as logs are monotonic)");
+Chart::new(180, 60, 0.0, 15.0)
+    .lineplot(Shape::Lines(&log_likelihood_progress))
+    .display();
+
+println!("Decision boundry after 25 iterations");
+decision_boundry(&weights);
+
+// The model should have learnt to classify class 1 correctly at the expected value
+// of the cluster
+assert!(
+    sigmoid(
+        weights.scalar_product(Tensor::from([("weights", 3)], vec![ 1.0, 2.0, 3.0]))
+    ) > 0.5);
+
+// The model should have learnt to classify class 2 correctly at the expected value
+// of the cluster
+assert!(
+    sigmoid(
+        weights.scalar_product(Tensor::from([("weights", 3)], vec![ 1.0, -2.0, -1.0]))
+    ) < 0.5);
+
+/**
+ * A utility function to plot the decision boundry of the model. As the terminal plotting
+ * library doesn't support colored plotting at the time of writing this is a little challenging
+ * to do given we have two dimensions of inputs and one dimension of output which is also real
+ * valued as logistic regression computes probability. This could best be done with a 3d
+ * plot or a heatmap, but is done with this function by taking 0.5 as the cutoff for
+ * classification, generating a grid of points in the two dimensional space and classifying all
+ * of them, then plotting the ones classified as class 1.
+ *
+ * TODO: work out how to retrieve a line from these points so the decision boundry is easier
+ * to see
+ */
+fn decision_boundry(weights: &Tensor<f64, 1>) {
+    // compute a matrix of coordinate pairs from (-8.0, -8.0) to (8.0, 8.0)
+    let grid_values = Tensor::empty([("x", 160), ("y", 160)], 0.0);
+    // create a matrix of tuples combining every combination of coordinates
+    let grid_values = grid_values.map_with_index(|[i, j], _| (
+        (i as f64 - 80.0) * 0.1, (j as f64 - 80.0) * 0.1)
+    );
+    // iterate through every tuple and see if the model predicts class 1
+    let points = grid_values.index_by(["y", "x"])
+        .iter()
+        .map(|(x1, x2)| {
+            let input = Tensor::from([("weights", 3)], vec![ 1.0, x1, x2 ]);
+            let prediction = weights.scalar_product(input);
             return if prediction > 0.5 {
                 (x1, x2, 1)
             } else {
